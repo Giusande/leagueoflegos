123c5869ed82a0e878ddc8ae62d8ea5576369179
Giusande <91056214+Giusande@users.noreply.github.com>
2022-03-31 22:36:04 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
-var bxMenu = document.querySelector('.bx-menu');
-var bxX = document.querySelector('.bx-x');
-var body = document.querySelector('body');
-var nav = document.querySelector('nav');
-
-bxMenu.addEventListener('click', function() {
-  nav.style.left = "0%"
-  body.style.overflow = "hidden"
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
 
-bxX.addEventListener('click', function() {
-  nav.style.left = "-100%"
-  body.style.overflow = "visible"
+$(window).scroll(function(){
+  if(this.scrollY > 20){
+    $("header").addClass("sticky");
+    var inibtn = document.querySelector('.up-arow');
+    inibtn.style.display = "block"
+  }
+  else{
+    $("header").removeClass("sticky");
+    var inibtn = document.querySelector('.up-arow');
+    inibtn.style.display = "none"
+  }
+});
+
+var inta = document.querySelector('.bxl-instagram');
+var twit = document.querySelector('.bxl-twitch');
+
+inta.addEventListener('click', function(){
+  window.location.href = "https://www.instagram.com/equipeleagueoflego/"
+});
+
+twit.addEventListener('click', function(){
+  window.location.href = "https://www.twitch.tv/leagueof_legos"
 });
